cb65adfbf285b90f585079b8fca1d559a4aec5d3
David Kaplan <david@2of1.org>
2011-02-21 10:36:17 +0200

    display: ncurses initial work

--- ldpreloader.c
+++ ldpreloader.c
@@ -2,8 +2,62 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <ncurses.h>
 
 #include "dynsym.h"
+
+#define N_FREE(X) \
+  do {            \
+    if( X ) {     \
+      free( X );  \
+      X = NULL;   \
+    }             \
+  } while( 0 )
+
+#define EXIT(X) \
+  do {          \
+    clear();    \
+    endwin();   \
+    exit( X );  \
+  } while( 0 )
+
+typedef struct display {
+  int rows,
+      cols;
+} DISPLAY;
+
+void _init_display(DISPLAY *d)
+{
+  initscr();
+  cbreak();
+  keypad( stdscr, TRUE );
+  curs_set( FALSE );
+  noecho();
+
+  start_color();
+
+  int bg_color = COLOR_BLACK;
+  if( use_default_colors() == OK )
+    bg_color = -1;
+
+  init_pair( 1, COLOR_CYAN, bg_color );
+  init_pair( 2, COLOR_WHITE, bg_color );
+  init_pair( 3, COLOR_GREEN, bg_color );
+  init_pair( 4, COLOR_RED, bg_color );
+  init_pair( 5, COLOR_YELLOW, bg_color );
+  init_pair( 6, COLOR_BLACK, COLOR_GREEN );
+  init_pair( 7, COLOR_BLUE, bg_color );
+  init_pair( 8, COLOR_WHITE, COLOR_BLUE );
+
+  clear();
+  getmaxyx( stdscr, d->rows, d->cols );
+}
+
+void _destroy_display(DISPLAY *d)
+{
+  clear();
+  endwin();
+}
 
 int main(int ac, char *av[])
 {
