44ec81ea44cb2d6cd2eb11dfdd5aec76feb2db30
David Kaplan <david@2of1.org>
2011-02-21 10:27:48 +0200

    first go at program flow description

--- ldpreloader.c
+++ ldpreloader.c
@@ -5,21 +5,28 @@
 
 #include "dynsym.h"
 
-/* test code */
 int main(int ac, char *av[])
 {
-    int fd = open( av[1], O_RDONLY );
+  /* test code */
+  int fd = open( av[1], O_RDONLY );
 
-    DYNSYM *ds = get_dynsyms( fd );
+  DYNSYM *ds = get_dynsyms( fd );
 
-    close( fd );
+  close( fd );
 
-    while( ds ) {
-        printf( "%s\n", ds->name );
-        ds = ds->nxt;
-    }
+  while( ds ) {
+      printf( "%s\n", ds->name );
+      ds = ds->nxt;
+  }
 
-    free_dynsyms( ds );
+  free_dynsyms( ds );
 
-    return 0;
+  /* add target to DB */
+  /* get dynamic symbols */
+  /* add symbols to DB ) */
+  /* (auto-resolve sigs and add to DB) */
+  /* pull symbols from DB and show symbol list (pull on every refresh - inefficient but probably quick) */
+  /* allow for selection of symbols */
+
+  return 0;
 }
